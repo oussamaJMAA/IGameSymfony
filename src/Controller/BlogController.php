625f33b8b0597385d0bb574b08bc787e6f527f45
rayanguedri <63672654+rayanguedri@users.noreply.github.com>
2022-04-25 01:46:47 +0100

    .

---
+++ src/Controller/BlogController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Publication;
+use App\Entity\Comment;
+use App\Repository\PublicationRepository;
+use App\Repository\CommentRepository;
+use DateTime;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Id;
+use app\Form\CommentFormType;
+use Symfony\Component\Validator\Constraints\DateTime as ConstraintsDateTime;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/admin", name="admin")
+     */
+    public function admin(): Response
+    {
+        
+        return $this->render('admin_template/base.html.twig');
+    }
+    
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index(PublicationRepository $repo): Response
+    {
+
+
+        $publications = $repo->findAll();
+        $comment = $repo->findAll();
+
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+            'publications' => $publications
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+
+    public function home()
+    {
+        return $this->render('blog/home.html.twig', [
+            'title' => "Bienvenue dans notre blog",
+            'age' => 31,
+        ]);
+    }
+
+    /**
+     * @Route("/blog/new", name="blog_create")
+     * @Route("/blog/{id}/edit" , name = "blof_edit")
+     */
+    public function form(Publication $publication = null, Request $request, EntityManagerInterface $manager)
+    {
+
+        if (!$publication) {
+
+            $publication = new Publication;
+        }
+
+
+        $form = $this->createFormBuilder($publication)
+            ->add('titre')
+            ->add('contenu')
+            ->add('image')
+            ->getForm();
+
+
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            if (!$publication->getId()) {
+                $publication->setCreatedAt(new \DateTimeImmutable());
+            }
+
+
+
+
+            $manager->persist($publication);
+            $manager->flush();
+
+            return $this->redirectToRoute('blog_show', ['id' => $publication->getId()]);
+        }
+
+
+        return $this->render('blog/create.html.twig', [
+            'formPublication' => $form->createView(),
+            'editMode' =>  $publication->getId() !== null
+        ]);
+    }
+
+
+
+
+    /**
+     * @Route("/blog/{id}", name="blog_show")
+     */
+    public function show(Publication $publication)
+    {
+
+        return $this->render('blog/show.html.twig', [
+            'publication' => $publication
+        ]);
+    }
+
+    /**
+     * @Route("/blog/show/{id}", name="delete_blog")
+     */
+    public function delete($id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $publication = $entityManager->getRepository(Publication::class)->find($id);
+        $entityManager->remove($publication);
+        $entityManager->flush();
+
+        return $this->redirectToRoute("blog");
+    }
+
+    /**
+     * @Route("/blog/neww", name="blog_createe")
+     */
+}
---
+++ src/Entity/Comment.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity(repositoryClass=CommentRepository::class)
+ */
+class Comment
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\NotEqualTo("bad")
+     */
+    private $auteur;
+
+    /**
+     * @ORM\Column(type="text")
+     * @Assert\NotBlank()
+     * @Assert\NotEqualTo("bad")
+     */
+    private $contenu;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     
+     */
+    private $createdAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Publication::class, inversedBy="comments")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $publication;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+        return $this;
+    }
+
+
+    public function getAuteur(): ?string
+    {
+        return $this->auteur;
+    }
+
+    public function setAuteur(string $auteur): self
+    {
+        $this->auteur = $auteur;
+
+        return $this;
+    }
+
+    public function getContenu(): ?string
+    {
+        return $this->contenu;
+    }
+
+    public function setContenu(string $contenu): self
+    {
+        $this->contenu = $contenu;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeImmutable
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeImmutable $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    public function getPublication(): ?Publication
+    {
+        return $this->publication;
+    }
+
+    public function setPublication(?Publication $publication): self
+    {
+        $this->publication = $publication;
+
+        return $this;
+    }
+
+    public function __toString() {
+        return strval($this->id);
+    }
+
+}
